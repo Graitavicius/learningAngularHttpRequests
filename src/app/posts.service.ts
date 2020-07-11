9053fb5e3e4a78e8c52226cd98b0d63041ad0b3b
Graitavicius <grait.mindaugas@gmail.com>
2020-07-11 18:13:37 +0300

    setting headers and adding query params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
@@ -27,9 +27,16 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("custom", "key");
     return this.http
       .get<{ [key: string]: Post }>(
-        "https://firstproject-fe71a.firebaseio.com/posts.json"
+        "https://firstproject-fe71a.firebaseio.com/posts.json",
+        {
+          headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+          params: searchParams,
+        }
       )
       .pipe(
         map((responseData) => {
